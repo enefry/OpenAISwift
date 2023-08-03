58464363472dd978fe2f0e17e64bd69360cb125e
chen <2153953+enefry@users.noreply.github.com>
2023-08-03 22:45:32 +0800

    1. fixed id not allow by openai 2. fixed error response will stream chat

--- Sources/OpenAISwift/ServerSentEventsHandler.swift
+++ Sources/OpenAISwift/ServerSentEventsHandler.swift
@@ -1,6 +1,6 @@
 //
 //  ServerSentEventsHandler.swift
-//  
+//
 //
 //  Created by Vic on 2023-03-25.
 //
@@ -8,18 +8,17 @@
 import Foundation
 
 class ServerSentEventsHandler: NSObject {
-    
     var onEventReceived: ((Result<OpenAI<StreamMessageResult>, OpenAIError>) -> Void)?
     var onComplete: (() -> Void)?
-    
+
     private lazy var session: URLSession = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
     private var task: URLSessionDataTask?
-       
+
     func connect(with request: URLRequest) {
         task = session.dataTask(with: request)
         task?.resume()
     }
-    
+
     func disconnect() {
         task?.cancel()
     }
@@ -35,12 +34,15 @@ class ServerSentEventsHandler: NSObject {
 }
 
 extension ServerSentEventsHandler: URLSessionDataDelegate {
-    
     /// It will be called several times, each time could return one chunk of data or multiple chunk of data
     /// The JSON look liks this:
     /// `data: {"id":"chatcmpl-6yVTvD6UAXsE9uG2SmW4Tc2iuFnnT","object":"chat.completion.chunk","created":1679878715,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{"role":"assistant"},"index":0,"finish_reason":null}]}`
     /// `data: {"id":"chatcmpl-6yVTvD6UAXsE9uG2SmW4Tc2iuFnnT","object":"chat.completion.chunk","created":1679878715,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{"content":"Once"},"index":0,"finish_reason":null}]}`
     func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        if let error = try? JSONDecoder().decode(ChatError.self, from: data) {
+            onEventReceived?(.failure(OpenAIError.chatError(error: error.error)))
+            return
+        }
         if let eventString = String(data: data, encoding: .utf8) {
             let lines = eventString.split(separator: "\n")
             for line in lines {
@@ -54,7 +56,7 @@ extension ServerSentEventsHandler: URLSessionDataDelegate {
             }
         }
     }
-    
+
     func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
         if let error = error {
             onEventReceived?(.failure(.genericError(error: error)))
